77089706688a06740cb8a4412ac72a7b26981de5
Ren Yu <jimmy12381456@gmail.com>
2021-06-06 14:10:48 +0800

    修改main

--- app/src/main/java/main/Main.java
+++ app/src/main/java/main/Main.java
@@ -2,11 +2,13 @@ package main;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class Main {
 
 	public static void main(String[] args) throws Throwable {
-		System.out.println("游泳池收費標準:\n"
+		 Logger logger = Logger.getLogger("loggingTest");
+		logger.info("游泳池收費標準:\n"
 				+ "(1) 一般票價 200 (2) 星期六日250元 (3) 12歲以下, 60歲(含)以上打八折, 限定 3-75 歲可入內游泳\n"
 				+ "(4) 七點以前八折 (營業時間:05:00-22:00) (5) 團體打七折 (6) 會員打五折 (7) 各打折不得重疊使用\n");
 		int age = Query.queryAge();
